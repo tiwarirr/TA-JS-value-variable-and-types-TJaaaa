ba2e85c4996713e357593bb6ba57fbc9f57659c4
Aparna <tiwari.aparna2899@gmail.com>
2021-10-04 00:32:28 +0530

    Assignment: Loops

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,39 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
-// Using `console.log` log all the evan values from 1 to 10.
+for(i=1; i<=10 ; i++){
+    console.log(i);
+}
 
-// Using `console.log` log all the od values from 1 to 10.
+// Using `console.log` log all the even values from 1 to 10.
+
+for(i=1; i<=10 ; i++){
+    if(i%2 == 0)
+    console.log(i);
+}
+
+
+// Using `console.log` log all the odd values from 1 to 10.
+
+for(i=1; i<=10 ; i++){
+    if(i%2 !== 0)
+    console.log(i);
+}
 
 // Calculate the sum of all numbers from 1 to 10.
 
+let sum = 0;
+for(i=1; i<=10 ; i++){
+    sum += i;
+}
+console.log(sum);
+
 // Log all the values from 1 to 10 using while loop
+
+let i = 1;
+while(i<=10)
+{
+    console.log(i);
+    i++;
+}
+
