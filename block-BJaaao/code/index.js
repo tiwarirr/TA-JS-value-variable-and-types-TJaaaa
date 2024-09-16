2c92d7fb6e32526a6082cd76795fb91276e4dcd5
RRT <tiwari.radharaman@gmail.com>
2024-09-16 15:46:28 +0530

    alert, confrim

--- block-BJaaao/code/index.js
+++ block-BJaaao/code/index.js
@@ -17,11 +17,14 @@ let num2 = 32;
 - Alert `The sum of [num1] and [num2] is [num1 + num1]`
 */
 
-let language = `JavaScript`;
-alert(`I am learning ${language}`);
+// let language = `JavaScript`;
+// alert(`I am learning ${language}`);
+// alert(`The value of num1 is ${num1} and the value of num2 is ${num2}`);
+// alert(`The sum of ${num1} and ${num2} is ${num1 + num1}`)
+let language = 'JavaScript';
+alert(`I'm learning ${language}.`);
 alert(`The value of num1 is ${num1} and the value of num2 is ${num2}`);
-alert(`The sum of ${num1} and ${num2} is ${num1 + num1}`)
-
+alert(`The sum of ${num1} and ${num2} is ${num1 + num2}`);
 /*
 2. Do the following using `alert`, `prompt` and `confirm` and `console`
 
@@ -37,20 +40,35 @@ alert(`The sum of ${num1} and ${num2} is ${num1 + num1}`)
     Adult: [isAdult]
     Profession: [userProfession]` (it should be in three different lines)
 */
+let userName = prompt('Enter User Name: ');
+let userProfession = prompt('Enter Profession: ');
+let isAdult = confirm('Are you Adult? ');
+alert(`I'm ${userName} a ${userProfession}.`);
+console.log(`I'm ${userName} a ${userProfession}.`);
+alert(`Username: ${userName} 
+  Adult: ${isAdult} 
+  Profession: ${userProfession}`); 
+console.log(`Username: ${userName} 
+  Adult: ${isAdult} 
+  Profession: ${userProfession}`); 
 
-let userName = prompt(`User Name`);
-let userProfession = prompt(`User Profession`);
-let isAdult = confirm(`Is user an adult?`);
-alert(`I am ${userName} a ${userProfession}`);
-console.log(`I am ${userName} a ${userProfession}`);
-alert(`Username: ${userName}
-Adult: ${isAdult}
-Profession: ${userProfession}
-`)
-console.log(`Username: ${userName}
-Adult: ${isAdult}
-Profession: ${userProfession}`
-);
+
+
+
+
+// let userName = prompt(`User Name`);
+// let userProfession = prompt(`User Profession`);
+// let isAdult = confirm(`Is user an adult?`);
+// alert(`I am ${userName} a ${userProfession}`);
+// console.log(`I am ${userName} a ${userProfession}`);
+// alert(`Username: ${userName}
+// Adult: ${isAdult}
+// Profession: ${userProfession}
+// `)
+// console.log(`Username: ${userName}
+// Adult: ${isAdult}
+// Profession: ${userProfession}`
+// );
 
 /*
 . Addition using prompt
@@ -62,10 +80,16 @@ Profession: ${userProfession}`
   - Log using console.log [numA - numB]
   - Log using console.log [numA * numB]
 */
+let numA = prompt('Enter Number A:');
+let numB = prompt('Enter Number B:');
+alert(`Sum both numbers is : ${Number(numA) + Number(numB)}`);
+alert(`numA-numB is : ${numA - numB}`);
+alert(`numA * numB is : ${numA - numB}`);
 
-let numA = prompt(`Number A`);
-let numB = prompt(`Number B`);
-alert(numA - numB);
-alert(numA * numB);
-console.log(numA - numB);
-console.log(numA * numB);
+
+// let numA = prompt(`Number A`);
+// let numB = prompt(`Number B`);
+// alert(numA - numB);
+// alert(numA * numB);
+// console.log(numA - numB);
+// console.log(numA * numB);
